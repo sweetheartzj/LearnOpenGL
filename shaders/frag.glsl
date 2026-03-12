72cca2d8fedb28c373683cec85900e9442c2e1be
zj <2426197840@qq.com>
2026-03-13 00:38:51 +0800

    texture

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,7 +1,11 @@
 #version 460 core
 out vec4 FragColor;
+
 in vec3 outColor;
+in vec2 TexCoord;
+
+uniform sampler2D outTexture;
 
 void main() {
-    FragColor = vec4(outColor, 1.0);
+    FragColor = texture(outTexture, TexCoord) * vec4(outColor, 1.0);
 }
